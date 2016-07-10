330b947bbf8fbb53d622d0e43b4f9684add8d14a
James Collins <j_collins@mac.com>
2016-07-10 11:15:05 +0100

    Working login page, but not yet able to login.

--- project/db_create.py
+++ project/db_create.py
@@ -5,3 +5,21 @@ with sqlite3.connect(DATABASE_PATH) as connection:
 
     # get a cursor object used to execute SQL commands
     c = connection.cursor()
+
+    # create the table
+    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY 
+        AUTOINCREMENT, name TEXT NOT NULL, due_date
+        TEXT NOT NULL, priority INTEGER NOT NULL, status
+        INTEGER NOT NULL)""")
+
+    # insert dummy data into the table
+
+    c.execute(
+        'INSERT INTO tasks (name, due_date, priority, status)'
+        'VALUES("Finish this tutorial", "01/09/2016", 10, 1)'
+        )
+    c.execute(
+        'INSERT INTO tasks (name, due_date, priority, status)'
+        'VALUES("Finish Real Python Course 2", "01/01/2017",\
+        10, 1)'
+        )
