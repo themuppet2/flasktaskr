d9190d7b2b8e81fc2151517aa1fbe3fb9be678f9
James Collins <j_collins@mac.com>
2016-07-17 11:03:08 +0100

    Added ability to register multiple users. These users all have CRUD access but task always allocated to first created user.

--- project/views.py
+++ project/views.py
@@ -1,8 +1,9 @@
-from forms import AddTaskForm
+from forms import AddTaskForm, RegisterForm, LoginForm
 from functools import wraps
 from flask import Flask, flash, redirect, render_template,\
-request, session, url_for, g
+request, session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+import datetime
 
 # config
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 app.config.from_object('_config')
 db = SQLAlchemy(app)
 
-from models import Task
+from models import Task, User
 
 
 def login_required(test):
@@ -33,16 +34,22 @@ def logout():
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
+    form = LoginForm(request.form)
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']\
-        or request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid Credentials, try again.'
-            return render_template('login.html', error=error)
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=request.form['name']).first()
+
+            if user is not None and user.password == request.form['password']:
+                session['logged_in'] = True
+                flash('Welcome!')
+                return redirect(url_for('tasks'))
+            else:
+                error = 'Invalid username or password.'
         else:
-            session['logged_in'] = True
-            flash('Welcome!')
-            return redirect(url_for('tasks'))
-    return render_template('login.html')
+            error = 'All fields required.'
+    return render_template('login.html', form=form,
+            error=error)
+        
 
 @app.route('/tasks/')
 @login_required
@@ -70,6 +77,8 @@ def new_task():
                 form.name.data,
                 form.due_date.data,
                 form.priority.data,
+                datetime.datetime.utcnow(),
+                '1',
                 '1'
                 )
             db.session.add(new_task)
@@ -99,3 +108,23 @@ def delete_entry(task_id):
     db.session.commit()
     flash('The task was deleted.')
     return redirect(url_for('tasks'))
+
+# Register
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_user = User(
+                form.name.data,
+                form.email.data,
+                form.password.data
+                )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Thanks for registering. Please login.')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form,
+        error=error)
