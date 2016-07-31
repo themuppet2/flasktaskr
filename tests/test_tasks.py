314d785d1adb8f2bd0ef64235ec676de7e7940c6
James Collins <j_collins@mac.com>
2016-07-31 17:24:21 +0100

    Now displays username in Tasks screen. Hidden links for users that cannot modify tasks and added relevant tests.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -166,5 +166,96 @@ class TasksTests(unittest.TestCase):
         response = self.app.get('delete/1/', follow_redirects=True)
         self.assertIn(b'The task was deleted.', response.data)
 
+    def test_task_template_displays_logged_in_user_name(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        response = self.app.get('tasks/', follow_redirects=True)
+        self.assertIn(b'user1234', response.data)
+
+    def test_users_cannot_see_task_modify_links_for_open_tasks_not_created_by_them(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.register(
+            'user5678', 'user5678@test.com', 'userpassword',
+            'userpassword'
+        )
+        response = self.login('user5678', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.assertNotIn(b'Mark as Complete', response.data)
+        self.assertNotIn(b'Delete', response.data)
+
+    def test_users_cannot_see_task_modify_links_for_closed_tasks_not_created_by_them(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.app.get('complete/1/', follow_redirects=True)
+        self.logout()
+        self.register(
+            'user5678', 'user5678@test.com', 'userpassword',
+            'userpassword'
+        )
+        response = self.login('user5678', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.assertNotIn(b'Delete', response.data)
+
+    def test_users_can_see_task_modify_links_for_tasks_created_by_them(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.create_task()
+        self.assertIn(b'complete/1/', response.data)
+        self.assertIn(b'delete/1/', response.data)
+
+    def test_admin_users_can_see_task_modify_links_for_all_open_tasks(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_admin_user()
+        self.login('Superman', 'powerful')
+        response = self.create_task()
+        self.assertIn(b'complete/1/', response.data)
+        self.assertIn(b'delete/1/', response.data)
+        self.assertIn(b'complete/2/', response.data)
+        self.assertIn(b'delete/2/', response.data)
+
+    def test_admin_users_can_see_task_modify_links_for_all_closed_tasks(self):
+        self.register(
+            'user1234', 'user1234@test.com', 'userpassword',
+            'userpassword'
+        )
+        self.login('user1234', 'userpassword')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.app.get('complete/1/')
+        self.logout()
+        self.create_admin_user()
+        self.login('Superman', 'powerful')
+        self.create_task()
+        response = self.app.get('complete/2/', follow_redirects=True)
+        self.assertIn(b'delete/1/', response.data)
+        self.assertIn(b'delete/2/', response.data)
+
+
 if __name__ == "__main__":
     unittest.main()
